cd3872241b96163ab919d6e442b92402fe7a44b4
mild-guy <106711129@gms.tcu.edu.tw>
2020-03-16 13:51:42 +0800

    change question position

--- C/001-TwoSum.c
+++ C/001-TwoSum.c
@@ -1,18 +1,4 @@
 // 1. Two Sum
-
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-	*returnSize=2;
-	int *ret=(int*)malloc(sizeof(int)*2);
-	int list[10000]={0}, flag=1;
-
-	for(int i=0;i<numsSize&&flag;i++){
-		for(int j=0;j<i;j++) if(list[j] == nums[i]) { ret[0]=j; ret[1]=i; flag=0; break; }
-		list[i]=target-nums[i];
-	}
-	return ret;
-}
-
 /*
 Question:
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -26,6 +12,17 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+	*returnSize=2;
+	int *ret=(int*)malloc(sizeof(int)*2);
+	int list[10000]={0}, flag=1;
+
+	for(int i=0;i<numsSize&&flag;i++){
+		for(int j=0;j<i;j++) if(list[j] == nums[i]) { ret[0]=j; ret[1]=i; flag=0; break; }
+		list[i]=target-nums[i];
+	}
+	return ret;
+}
 /*
 result:
 Runtime: 68 ms, faster than 80.59% of C online submissions for Two Sum.
--- JavaScript/001-TwoSum.js
+++ JavaScript/001-TwoSum.js
@@ -1,4 +1,17 @@
 // 1. Two Sum
+/*
+Question:
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
 
 /**
  * @param {number[]} nums
@@ -17,21 +30,8 @@ var twoSum = function(nums, target) {
     }
     return ret;
 };
-
 /*
-Question:
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-*/
-/*
+Result:
 Runtime: 52 ms, faster than 93.64% of JavaScript online submissions for Two Sum.
 Memory Usage: 37 MB, less than 8.68% of JavaScript online submissions for Two Sum.
 */
